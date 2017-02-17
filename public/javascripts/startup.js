153182d2e3b5cc1e66a966ed78b4360ce81d3660
Susan Luong <sluong@klick.com>
2017-02-16 23:27:47 -0500

    more updates

--- public/javascripts/startup.js
+++ public/javascripts/startup.js
@@ -1,18 +1,33 @@
-function loadArticles(articles) {
+var _ = require('lodash');
 
+function displayArticles(articles) {
+	$('#heroSection').empty();
+	$('#articleSection').empty();
 
+	_.each(articles, function(article, index) {
+		var articleBlock = (index === 0) ? $('<div class="jumbotron">') : $('<div class="col-xs-6 col-lg-4">');
+		var title = $('<h1>').text(article.title);
+		var description = $('<p>').text(article.description);
 
+		title.appendTo(articleBlock);
+		description.appendTo(articleBlock);
+
+		if (index === 0) {
+			articleBlock.appendTo('#heroSection');
+		} else {
+			articleBlock.appendTo('#articleSection');
+		}
+	});
 };
+
+function getUpdatedContent(currentActivities) {
+	$.post('http://localhost:3000/articles/action/:actionId', currentActivities, function(response) {
+	    document.cookie.activities = response.activities
+	    displayArticles(response.articles);
+	}, 'json');
+}
 
 $(document).ready(function () {
 	// get initial data with existing activities list and no action
-	$.ajax({
-	  type: "POST",
-	  url: "localhost:3000/articles/action/:actionId",
-	  data: {},
-	  success: loadArticles(data),
-	  dataType: dataType
-	});
-
-
+	getUpdatedContent(document.cookie.activities);
 });
