b4666d6df49ccf1406fa9b4fa323e3c048054675
Susan Luong <sluong@klick.com>
2017-02-20 23:14:13 -0500

    small improvement

--- app/contentManager.js
+++ app/contentManager.js
@@ -1,9 +1,9 @@
 var _ = require('lodash');
 
+
 var ContentManager = function(articles) {
 	this.articles = articles;
 };
-
 
 var applyWeightToArticle = function(articlesList, articleId, weight) {
 	return _.forEach(articlesList, function(article) {
@@ -37,14 +37,11 @@ ContentManager.prototype.applyWeightToContent = function(ruleKey, weight) {
 	// apply weight to article, section or category depending on the number of parts in the content Id
 	switch (ruleKeyParts.length) {
 		case 1:
-			this.articles = applyWeightToCategory(this.articles, ruleKey, weight);
-			break;
+			return applyWeightToCategory(this.articles, ruleKey, weight);
 		case 2:
-			this.articles = applyWeightToSection(this.articles, ruleKey, weight);
-			break;
+			return applyWeightToSection(this.articles, ruleKey, weight);
 		case 3:
-			this.articles = applyWeightToArticle(this.articles, ruleKey, weight);
-			break;
+			return applyWeightToArticle(this.articles, ruleKey, weight);
 	};
 
 	return this.articles;
